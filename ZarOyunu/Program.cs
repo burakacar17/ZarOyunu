f14736314ef1e7aa0510ad954c4f81559d4dc2c1
Burak Acar <buacar17@gmail.com>
2022-04-23 23:22:55 +0300

    Zar oyunu oluşturuldu

---
+++ ZarOyunu/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ZarOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZarAt zarAt = new ZarAt(6);
+
+            Console.WriteLine(zarAt.go());
+            Console.WriteLine(zarAt.go());
+            Console.WriteLine(zarAt.go());
+            Console.WriteLine(zarAt.go());
+            Console.WriteLine(zarAt.go());
+            Console.WriteLine(zarAt.go());
+            Console.WriteLine(zarAt.go());
+            Console.WriteLine(zarAt.go());
+            Console.WriteLine(zarAt.go());
+        }
+    }
+}
---
+++ ZarOyunu/ZarAt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZarOyunu
+{
+    class ZarAt
+    {
+        int maxZarNumber;
+
+        public ZarAt(int maxZarNumber)
+        {
+            this.maxZarNumber = maxZarNumber;
+        }
+
+       public int go()
+        {
+            Random random = new Random();
+            int rnd = random.Next(1, maxZarNumber + 1);
+            return rnd;
+        }
+
+
+    }
+}
